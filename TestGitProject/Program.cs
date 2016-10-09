d57f9b3d20616a174c2d7207184b7d4821292183
ozkan35selek <ozkan35selek@gmail.com>
2016-10-09 13:17:37 +0300

    ......

--- TestGitProject/Program.cs
+++ TestGitProject/Program.cs
@@ -13,7 +13,7 @@ namespace TestGitProject
         /// </summary>
         [STAThread]
         static void Main()
-        {
+        { 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
